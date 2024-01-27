4ab8df8bc3096bb9007485dc64fc75a133149e39
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-01-27 16:12:38 +0100

    Update works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,12 +5,33 @@ import works from "../data/works.json";
 function Works(props) {
   const location = useLocation();
   const pathname = location.pathname;
+  const project = pathname.replace("/", "");
 
-  return (
-    <section>
-      <h1>{pathname}</h1>
-    </section>
-  );
+  var findIndexInWorks = (works, project) => {
+    var WorksCol = works.findIndex(function (element) {
+      return element.link === project;
+    });
+    if (WorksCol !== -1) {
+      return WorksCol;
+    }
+    return -1;
+  };
+
+  
+  var index = findIndexInWorks(works, project);
+  if (index != -1) {
+    return (
+      <section>
+        <h1>{project}</h1>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h1>Projet : {project} indisponible </h1>
+      </section>
+    );
+  }
 }
 
 export default Works;
